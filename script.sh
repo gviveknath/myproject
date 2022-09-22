07346045cc4d4eb058d02f881760b415098cb8ac
EC2 Default User <ec2-user@ip-172-31-18-112.us-west-1.compute.internal>
2022-09-22 14:52:56 +0000

    myfirst commit

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+f1 ()
+{
+echo "The top command shows a real-time view of running processes in Linux":
+        top
+}
+f2 ()
+{
+       echo "the amount of disk space available on the filesystem with each file name's argument":
+        df -H
+}
+
+f3 () {
+
+   name1=tata
+   name2=bye
+   name3=bye
+       echo $name1
+       echo $name2
+       echo $name3
+}
+
+f4 ()
+{
+        echo "Last 10 log messages shows":
+        sudo grep error  /var/log/messages
+}
+
+
+f1
+f2
+f3
+f4
